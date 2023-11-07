a61ea3051b543ceb63127fc8b681fda0a10a5aec
vedant29 <vedantk29@gmail.com>
2023-11-06 19:15:00 -0700

    Batman page

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,6 +26,10 @@ export default function Home() {
             <h3>Example About Me Page &rarr;</h3>
             <p>Click here to navigate to an example page</p>
           </a>
+          <a href='/Batman ' className='card'>
+            <h3>Example Batman Page &rarr;</h3>
+            <p>Click here to navigate to the Batman page</p>
+          </a>
 
           <a
             href='https://github.com/vedant29/Project-CSCI-1000'
